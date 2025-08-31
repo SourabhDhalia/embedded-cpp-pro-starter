b1dd3880187ea536784cd58b2714e53c664212dd
Sourabh Dhalia <sdhalia.007@gmail.com>
2025-08-31 18:44:00 +0530

    ci/style: enforce -style=file for clang-format checks and scripts; ignore cppcheck report

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 FILES=$(git ls-files '*.c' '*.cpp' '*.h' '*.hpp')
-[ -z "$FILES" ] || clang-format -i $FILES
-
+[ -z "$FILES" ] || clang-format -style=file -i $FILES
